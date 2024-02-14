14c23a674e376bf735b27ab88947ba682f42b9d2
k8mcelhnaey <kqmcelhaney@gmail.com>
2024-02-14 13:59:03 -0700

    commented added in routes/skills for new

--- routes/skills.js
+++ routes/skills.js
@@ -14,5 +14,8 @@ const skillsCtrl = require('../controllers/skills')
 router.get('/', skillsCtrl.index);
 
 //:id is calleda request paremeter
+//GET /todos
 router.get('/:id', skillsCtrl.show);
+//GET /todos/:id
 module.exports = router;
+// GET /todo/new <--this new route cannot stay here!
